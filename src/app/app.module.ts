987449f22369d7f3f02fc760aea3751640dc725f
Tairone Livinalli <taironelivinalli@hotmail.com>
2018-08-04 11:26:02 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,12 @@ import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/dat
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDheoECo-hpHR5sRxaBa6Mfhsa7FZ_vT48",
-  authDomain: "controle-3e1e4.firebaseapp.com",
-  databaseURL: "https://controle-3e1e4.firebaseio.com",
-  projectId: "controle-3e1e4",
-  storageBucket: "controle-3e1e4.appspot.com",
-  messagingSenderId: "63452964766"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  databaseURL: "your-database-url",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id"
 };
 @NgModule({
   declarations: [
